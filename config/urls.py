c6a1e5c00de49166f4ecf434d234d87494aaebb3
Berkay <berkaykhrmn@icloud.com>
2026-02-25 13:48:49 +0300

    Update API paths in urls.py for consistency

--- config/urls.py
+++ config/urls.py
@@ -18,13 +18,11 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', api_root, name='api_root'),
     path('api/', include(router.urls)),
-
-    path('user/', include('users.urls')),
-    path('cart/', include('carts.urls')),
-    path('orders/', include('orders.urls')),
-
-    path('schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('docs/', SpectacularSwaggerView.as_view(url_name='schema')),
+    path('api/user/', include('users.urls')),
+    path('api/cart/', include('carts.urls')),
+    path('api/orders/', include('orders.urls')),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema')),
 ]
 
 if settings.DEBUG:
